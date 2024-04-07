f8067e2aab835c3abf014cb6121ef6b25e190987
Chayanon Tonsai <maxjunerd@gmail.com>
2024-04-07 07:32:30 +0700

    url

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,5 +1,18 @@
 import { NextResponse } from "next/server";
-import data from "@/data.json";
+// import data from "@/data.json";
+
+const data = [
+  {
+    title: "Max",
+    description: "Amazing Post",
+    id: 1,
+  },
+  {
+    title: "Mild",
+    description: "Amazing Post",
+    id: 2,
+  },
+];
 
 export async function GET(request: Request, context: any) {
   const { params } = context;
